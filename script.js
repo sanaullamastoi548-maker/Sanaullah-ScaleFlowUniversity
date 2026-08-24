f8828930bab6f7bf6b7e17c2a27932402be96738
sanaullamastoi548-maker <sanaullamastoi548@gmail.com>
2026-08-24 23:52:04 +0300

    Fix URL format in SCALEFLOW_API_CONFIG

--- script.js
+++ script.js
@@ -16,7 +16,7 @@
         const SCALEFLOW_API_CONFIG = {
 
         WEB_APP_URL:
-            "https://script.google.com/macros/s/AKfycbzi-xw_9P5h9xvrG-WzmbCub6tR9PVBAxP98WyFyUd-XKIWvwziedjFaEvP9JPQ_GUy/exec",
+            "۔https://script.google.com/macros/s/AKfycbzi-xw_9P5h9xvrG-WzmbCub6tR9PVBAxP98WyFyUd-XKIWvwziedjFaEvP9JPQ_GUy/exec",
 
         VERSION:
             "v1",
